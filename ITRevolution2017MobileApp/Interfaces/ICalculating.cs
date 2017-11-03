b19c21cf1c4677a39d732e014c896360fe68a54e
Дмитрий <dku@cdm.dk>
2017-11-03 15:05:52 +0200

    Add project files.

---
+++ ITRevolution2017MobileApp/Interfaces/ICalculating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITRevolution2017MobileApp.Interfaces
+{
+    public interface ICalculating
+    {
+    }
+}
